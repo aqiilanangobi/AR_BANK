0ab2d3157c9f18fef006889ced57994712811d6c
aqiilanangobi <nangobiaqiila246@gmail.com>
2025-10-13 12:51:31 +0300

    We added models to our project and added them to the admin site

--- ar_bank/admin.py
+++ ar_bank/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Branch,Account_type,Customer,Account,Employee,Transaction,Transfer,Withdraw,Deposit
+
+class BranchAdmin(admin.ModelAdmin):
+    list_display =("name","location")
+admin.site.register(Branch,BranchAdmin)
+
+
+class Account_typeAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+admin.site.register(Account_type,Account_typeAdmin)
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ("first_name","last_name","address","contact")
+admin.site.register(Customer,CustomerAdmin)
+
+class AccountAdmin(admin.ModelAdmin):
+    list_display = ("opening_date","branch","customer","pin","balance","account_type","account_status")
+admin.site.register(Account,AccountAdmin)
+
+class EmployeeAdmin(admin.ModelAdmin):
+    list_display = ("first_name","last_name","employee_position","city","contact","gender")
+admin.site.register(Employee,EmployeeAdmin)
+
+
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ("date","amount","transaction_type","account","recorded_by")
+admin.site.register(Transaction,TransactionAdmin)
+
+class TransferAdmin(admin.ModelAdmin):
+    list_display = ("date","account","transfer_amount","receivers_account","recorded_by")
+admin.site.register(Transfer,TransferAdmin)
+
+class WithdrawAdmin(admin.ModelAdmin):
+    list_display = ("date","account","withdraw_amount","recorded_by")
+admin.site.register(Withdraw,WithdrawAdmin)
+
+class DepositAdmin(admin.ModelAdmin):
+    list_display = ("date","account","deposit_amount","recorded_by")
+admin.site.register(Deposit,DepositAdmin)
--- ar_bank/models.py
+++ ar_bank/models.py
@@ -1,3 +1,101 @@
 from django.db import models
 
 # Create your models here.
+
+class Branch(models.Model):
+    name = models.CharField(max_length=50,verbose_name="Branch Name")
+    location = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return f"{self.name}"
+    
+
+class Account_type(models.Model):
+    name = models.CharField(max_length=50,verbose_name="Account type ")
+    
+    def __str__(self):
+        return f"{self.name}"
+        
+    
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    address = models.CharField(max_length=100,null=True,blank=True,default="N/A")
+    contact = models.CharField(max_length=10)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+class Account(models.Model):
+    ACCOUNT_STATUS = [
+        ("A","Active"),
+        ("I","Inactive"),
+        ("S","Suspended"),
+    ]
+    opening_date = models.DateField(auto_now=True)
+    pin = models.CharField(max_length=20)
+    balance = models.IntegerField()
+    branch = models.ForeignKey(Branch,on_delete=models.CASCADE)
+    customer = models.ForeignKey(Customer,on_delete=models.CASCADE)
+    account_type = models.ForeignKey(Account_type,on_delete=models.CASCADE)
+    account_status = models.CharField(max_length=2,choices = ACCOUNT_STATUS)
+
+    def __str__(self):
+        return f"{self.customer} Account"
+
+
+class Employee(models.Model):
+    GENDER = [
+        ("F","Female"),
+        ("M","Male"),
+    ]
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    employee_position = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+    contact = models.CharField(max_length=10)
+    gender =models.CharField(max_length=2,choices=GENDER)
+
+    def __str__(self):
+        return f"{self.first_name} {self.employee_position}"
+
+class Transaction(models.Model):
+    date = models.DateField(auto_now=False)
+    amount= models.IntegerField(verbose_name="transaction_amount")
+    transaction_type = models.CharField(max_length=30)
+    account = models.ForeignKey(Account,on_delete=models.CASCADE)
+    recorded_by = models.ForeignKey(Employee,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.account} transaction"
+
+class Transfer(models.Model):
+    date = models.DateField(auto_now=False)
+    account = models.ForeignKey(Account,on_delete=models.CASCADE)
+    transfer_amount = models.IntegerField()
+    receivers_account = models.IntegerField()
+    recorded_by = models.ForeignKey(Employee,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.account} transfer"
+
+class Withdraw(models.Model):
+    date = models.DateField(auto_now=False)
+    account = models.ForeignKey(Account,on_delete=models.CASCADE)
+    withdraw_amount = models.IntegerField()
+    recorded_by = models.ForeignKey(Employee,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.account} Withdraw"
+
+class Deposit(models.Model):
+    date = models.DateField(auto_now=False)
+    account = models.ForeignKey(Account,on_delete=models.CASCADE)
+    deposit_amount = models.IntegerField()
+    recorded_by = models.ForeignKey(Employee,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.account} Deposit"
+
+
+
